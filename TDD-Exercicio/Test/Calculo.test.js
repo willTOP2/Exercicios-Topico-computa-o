95e984d5afcb0210ec0c6c1f79496ec70e7f08b2
Willian <willian.rodrygues146@gmail.com>
2021-07-09 12:21:55 -0300

    2- Teste Passando

--- TDD-Exercicio/Test/Calculo.test.js
+++ TDD-Exercicio/Test/Calculo.test.js
@@ -10,6 +10,56 @@ describe('Calculo', () => {
     
 }); 
 
+test('Funcionario desenvolvedor salario menor que 3000', () => {
+  
+    expect(calc.Calc("desenvolvedor", 2500)).toBe(2250)
+    
+}); 
+
+
+test('Funcionario DBA salario maior igual 2000', () => {
+  
+    expect(calc.Calc("DBA", 2500)).toBe(1875)
+    
+}); 
+
+
+test('Funcionario DBA salario menor que 2000', () => {
+  
+    expect(calc.Calc("DBA", 1800)).toBe(1530)
+    
+}); 
+
+
+
+test('Funcionario Testador salario maior igual 2000', () => {
+  
+    expect(calc.Calc("testador", 2500)).toBe(1875)
+    
+}); 
+
+
+test('Funcionario Testador salario menor que 2000', () => {
+  
+    expect(calc.Calc("testador", 1800)).toBe(1530)
+    
+}); 
+
+test('Funcionario Gerente salario maior igual 5000', () => {
+  
+    expect(calc.Calc("gerente", 5000)).toBe(3500)
+    
+}); 
+
+
+test('Funcionario Gerente salario menor que 500', () => {
+  
+    expect(calc.Calc("gerente", 4000)).toBe(3200)
+    
+}); 
+
+
+
 
 
 }); 
--- TDD-Exercicio/lib/Calculos.js
+++ TDD-Exercicio/lib/Calculos.js
@@ -3,7 +3,76 @@
 class Calculos {
 
    Calc(cargo,salario){
-       
+
+    if(cargo === 'desenvolvedor'){
+
+        if(salario >= 3000){
+     
+        var porcentagem = 20 * salario /100 
+        return salario - porcentagem
+        }
+    else{
+
+            var porcentagem = 10 * salario/100 
+            return salario- porcentagem
+         }
+        
+  } 
+
+
+    if(cargo === 'DBA'){
+
+      if(salario>=2000){
+
+
+        var porcentagem = 25* salario/100 
+        return salario- porcentagem
+      } else{
+
+
+        var porcentagem = 15 * salario/100 
+        return salario- porcentagem
+      }
+
+    }
+
+    if(cargo === 'testador'){
+
+        if(salario>=2000){
+  
+  
+          var porcentagem = 25* salario/100 
+          return salario- porcentagem
+        } else{
+  
+  
+          var porcentagem = 15 * salario/100 
+          return salario- porcentagem
+        }
+  
+      }
+  
+      if(cargo === 'gerente'){
+
+        if(salario>=5000){
+  
+  
+          var porcentagem = 30* salario/100 
+          return salario- porcentagem
+        } else{
+  
+  
+          var porcentagem = 20 * salario/100 
+          return salario- porcentagem
+        }
+  
+      }
+  
+
+
+
+
+
    }
 
 
